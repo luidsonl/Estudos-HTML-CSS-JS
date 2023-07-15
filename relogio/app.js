5da1ad26cb0d312eb5a3d4553087e9e6057a5cf9
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-07-14 22:13:21 -0300

    Adicionando marcação das horas

--- relogio/app.js
+++ relogio/app.js
@@ -1,6 +1,7 @@
 const secondsHand = document.getElementById('seconds-hand')
 const minutesHand = document.getElementById('minutes-hand')
 const hoursHand = document.getElementById('hours-hand')
+const markers = document.getElementById('markers')
 
 function getTime(){
 	const now = new Date()
@@ -10,6 +11,22 @@ function getTime(){
 
 	secondsHand.style.transform = `rotate(${second * 6}deg)`
 	minutesHand.style.transform = `rotate(${minute * 6}deg)`
-	hoursHand.style.transform = `rotate(${hour * 15}deg)`
+	hoursHand.style.transform = `rotate(${hour * 30}deg)`
 }
 setInterval(getTime, 100)
+
+function renderMarkers(){
+	for(let i = 0; i < 12; i++){
+		const marker = document.createElement('div')
+		if (i == 0){
+			marker.textContent = 12
+		} else{
+			marker.textContent = i
+		}
+		marker.style.transform = `rotate(${30 * i}deg)`
+
+		markers.appendChild(marker)
+	}
+}
+
+renderMarkers()
